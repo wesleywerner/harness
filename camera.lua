46c44e21d793b9482f978205904e37cfcfcea57e
Wesley <wesley.werner@gmail.com>
2017-12-02 14:57:27 +0200

    Add camera with example and documentation.

---
+++ camera.lua
@@ -0,0 +1,176 @@
+--[[
+   camera.lua
+
+   Copyright 2017 wesley werner <wesley.werner@gmail.com>
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program. If not, see http://www.gnu.org/licenses/.
+
+]]--
+
+--- Provides a camera to limit drawing to arbitrary positions.
+-- @module camera
+
+local module = {
+
+    -- a value 0..1 indicating progress of the camera start..end position
+    frames = 0,
+
+    -- the time in seconds the movement should take
+    time = 1,
+
+    -- camera goal on screen
+    targetX = 0,
+    targetY = 0,
+
+    -- camera actual on screen
+    x = 0,
+    y = 0,
+
+    -- camera frame size and position
+    frameWidth = love.graphics.getWidth( ),
+    frameHeight = love.graphics.getHeight( ),
+    frameTop = 0,
+    frameLeft = 0,
+
+    worldWidth = love.graphics.getWidth( ),
+    worldHeight = love.graphics.getHeight( ),
+
+}
+
+local function clamp(x, min, max)
+  return x < min and min or (x > max and max or x)
+end
+
+local function lerp(a, b, amount)
+  return a + (b - a) * clamp(amount, 0, 1)
+end
+
+local function distance(x1, y1, x2, y2, squared)
+  local dx = x1 - x2
+  local dy = y1 - y2
+  local s = dx * dx + dy * dy
+  return squared and s or math.sqrt(s)
+end
+
+--- Updates camera motion
+--
+-- @tparam number dt
+function module:update(dt)
+
+    if (self.x ~= self.targetX) or (self.y ~= self.targetY) then
+        self.frames = self.frames + (dt / self.time)
+
+        -- move the camera
+        self.x = lerp(self.fromX, self.targetX, self.frames)
+        self.y = lerp(self.fromY, self.targetY, self.frames)
+
+    end
+
+end
+
+--- Set the world size that the camera can scroll within.
+-- Camera movement is constrained within this size.
+--
+-- @tparam number width
+-- @tparam number height
+function module:worldSize(width, height)
+
+    self.worldWidth = width
+    self.worldHeight = height
+
+end
+
+--- Set the size of the frame visible through the camera.
+--
+-- @tparam number left
+-- @tparam number top
+-- @tparam number width
+-- @tparam number height
+function module:frame(left, top, width, height)
+
+    self.frameLeft = left
+    self.frameTop = top
+    self.frameWidth = width
+    self.frameHeight = height
+
+end
+
+--- Scroll the camera to a position in it's world.
+--
+-- @tparam number x
+-- @tparam number y
+function module:lookAt(x, y)
+
+    -- we only update the movement if the target is new
+    if self.targetX ~= x or self.targetY ~= y then
+        self.frames = 0
+        self.fromX = self.x
+        self.fromY = self.y
+
+        -- clamp to the world
+        self.targetX = clamp(x, - self.worldWidth + self.frameWidth, 0 )
+        self.targetY = clamp(y, - self.worldHeight + self.frameHeight, 0 )
+
+        -- the time taken to move the camera is a function of distance over the smallest world side.
+        -- this means smaller movements happen slower.
+        local dist = distance(self.x, self.y, x, y, false) / math.min( self.worldHeight, self.worldWidth )
+        dist = math.exp(dist)
+        -- limit the time to sane values
+        self.time = clamp(dist, 0.5, 6)
+    end
+
+end
+
+--- Move the camera position relative to it's current position.
+--
+-- @tparam number dx
+-- @tparam number dy
+function module:moveBy(dx, dy)
+
+   self:lookAt(self.targetX + dx, self.targetY + dy)
+
+end
+
+--- Center the camera frame on a position.
+--
+-- @tparam number x
+-- @tparam number y
+function module:center(x, y)
+
+    local dx = - x + (self.frameWidth / 2)
+    local dy = - y + (self.frameHeight / 2)
+    self:lookAt(dx, dy)
+
+end
+
+--- Prepare for drawing the contents of the camera world
+-- This applies transforms and clipping.
+function module:pose()
+
+    love.graphics.push()
+    love.graphics.translate(self.x + self.frameLeft, self.y + self.frameTop)
+    love.graphics.setScissor( self.frameLeft, self.frameTop, self.frameWidth, self.frameHeight )
+
+end
+
+--- Relax the camera when done drawing world contents.
+-- This releases transform and clipping, restoring normal drawing state.
+function module:relax()
+
+    love.graphics.setScissor()
+    love.graphics.pop()
+
+end
+
+return module
---
+++ examples/camera-example.lua
@@ -0,0 +1,90 @@
+--[[
+   camera-example.lua
+
+   Copyright 2017 wesley werner <wesley.werner@gmail.com>
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program. If not, see http://www.gnu.org/licenses/.
+
+]]--
+
+local camera = require("harness.camera")
+local scale = 5
+
+function love.load()
+
+    love.graphics.setDefaultFilter( "nearest", "nearest", 1 )
+
+    background = love.graphics.newImage( "swamp-300px.png" )
+
+    -- camera world size is our image size
+    camera:worldSize(background:getWidth() * scale, background:getHeight() * scale)
+
+    -- frame is what we see at any one time
+    camera:frame(20, 20, 400, 400)
+
+    love.graphics.setFont( love.graphics.newFont( 10 ) )
+
+end
+
+function love.update(dt)
+
+    camera:update(dt)
+
+end
+
+function love.draw()
+
+    -- pose the camera. everything drawn now is clipped and framed.
+    camera:pose()
+
+    -- scale up
+    love.graphics.scale(scale, scale)
+
+    -- draw the image
+    love.graphics.draw(background)
+
+    -- print image credits
+    love.graphics.printf("Frog On Bluish Pond by schugschug (openclipart.org/detail/117031/frog-on-bluish-pond)", 0, 0, 400, "center")
+
+    -- relax the camera. drawing returns to normal
+    camera:relax()
+
+    -- draw a box around the camera frame
+    love.graphics.rectangle("line", camera.frameLeft, camera.frameTop,
+    camera.frameWidth, camera.frameHeight)
+
+    -- print some help text
+    love.graphics.print("use arrow keys to scroll, a/d to switch the frame position")
+
+end
+
+function love.keypressed(key)
+
+    if key == "escape" then
+        love.event.quit()
+    elseif key == "left" then
+        camera:moveBy(100, 0)
+    elseif key == "right" then
+        camera:moveBy(-100, 0)
+    elseif key == "up" then
+        camera:moveBy(0, 100)
+    elseif key == "down" then
+        camera:moveBy(0, -100)
+    elseif key == "a" then
+        camera:frame(20, camera.frameTop, camera.frameWidth, camera.frameHeight)
+    elseif key == "d" then
+        camera:frame(300, camera.frameTop, camera.frameWidth, camera.frameHeight)
+    end
+
+end
